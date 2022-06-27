cbd7205d498738d6b5e1b78d586e57cfe2817357
Vitaliy Malyshev <vitaliymalyshev@team.asana.com>
2022-06-27 13:05:16 -0400

    lazyload

--- src/components/gallery/index.js
+++ src/components/gallery/index.js
@@ -3,15 +3,38 @@ import Preview from "./../preview";
 import GalleryImage from "./../gallery-image";
 import styles from './styles.module.scss'
 
+const PAGE_SIZE = 50;
+const NEXT_PAGE_THRESHOLD = 30;
+const ROW_SIZE = 5;
+const ROW_HEIGHT = 150;
 
 function Gallery() {
     const [preview, setPreview] = useState(false);
     const [current, setCurrent] = useState(0);
     const [files, setFiles] = useState([]);
 
+    const [visibleFiles, setVisibleFiles] = useState([]);
+
     useEffect(() => {
         setFiles(window.initialState.map((item, id) => ({ ...item, id })));
     }, []);
+
+
+    useEffect(() => {
+        setVisibleFiles(files.slice(0, PAGE_SIZE));
+    }, [files]);
+
+    useEffect(() => {
+        const cb = () => {
+            if (files.length > visibleFiles.length) {
+                 if (window.scrollY > ( visibleFiles.length - NEXT_PAGE_THRESHOLD) / ROW_SIZE * ROW_HEIGHT) {
+                     setVisibleFiles([...visibleFiles, ...files.slice(visibleFiles.length, visibleFiles.length + PAGE_SIZE)]);
+                }
+            }
+        }
+        window.addEventListener('scroll', cb);
+        return () => window.removeEventListener('scroll', cb);
+    })
 
     const onClick = (index) => {
         setPreview(true);
@@ -20,7 +43,6 @@ function Gallery() {
     const onClose = () => {
         setPreview(false);
     }
-
     const onNext = () => {
         const next = current + 1;
         if (files[next]) setCurrent(next);
@@ -31,14 +53,13 @@ function Gallery() {
         if (files[prev]) setCurrent(prev);
         else setCurrent(files.length - 1);
     }
-
     const onSetCurrent = (id) => {
         setCurrent(files.findIndex(i => i.id === id));
     }
 
     return (
         <div className={styles.grid}>
-            {files.map((file, i) => {
+            {visibleFiles.map((file, i) => {
                 return (<div className={styles.item} key={i} onClick={() => onClick(i)}>
                     <GalleryImage name={file.name} url={file.preview_url || file.url} />
                     <div className={styles.overlay}>{file.name}</div>
